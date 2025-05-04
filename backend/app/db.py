fdf530ecbdd48a9d839c457d818a8b0b8f6fed8c
Tu Nguyen <minhtucs@gmail.com>
2025-05-04 10:49:22 +0700

    Add docker compose file

--- backend/app/db.py
+++ backend/app/db.py
@@ -5,15 +5,15 @@ import os
 load_dotenv()
 
 db_config = (
-  f"host={os.environ['POSTGRESQL_HOST']} "
-  f"port={os.environ['POSTGRESQL_PORT']} "
-  f"user={os.environ['POSTGRESQL_USER']} "
-  f"password={os.environ['POSTGRESQL_PASSWORD']} "
-  f"dbname={os.environ['POSTGRESQL_DB']}"
+  f"host={os.environ['POSTGRES_HOST']} "
+  f"port={os.environ['POSTGRES_PORT']} "
+  f"user={os.environ['POSTGRES_USER']} "
+  f"password={os.environ['POSTGRES_PASSWORD']} "
+  f"dbname={os.environ['POSTGRES_DB']}"
 )
 
-MIN_CONN = int(os.environ['POSTGRESQL_MIN_CONN'])
-MAX_CONN = int(os.environ['POSTGRESQL_MAX_CONN'])
+MIN_CONN = int(os.environ['POSTGRES_MIN_CONN'])
+MAX_CONN = int(os.environ['POSTGRES_MAX_CONN'])
 
 pool = ConnectionPool(conninfo=db_config, min_size=MIN_CONN, max_size=MAX_CONN)
 
